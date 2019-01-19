1e7da82eda4511e5179fbb7ae846722042528af7
王明欢 <73226349@qq>
2019-01-19 10:09:47 +0800

    提交

---
+++ pages/gathering/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      活动列表
+      <div v-for="(item,index) in items" :key="index">
+        <nuxt-link :to="'/gathering/item/'+item.id">{{item.name}}</nuxt-link>
+      </div>
+    </div>
+</template>
+
+<script>
+  import axios from 'axios'
+    export default {
+        asyncData(){
+          return axios.get('https://www.easy-mock.com/mock/5c3c576683a6d6013cad25cd/gathering/gathering').then(res=>{
+            return {items: res.data.data}
+          })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/gathering/item/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    活动详情
+    {{item.id}}
+    <hr/>
+    {{item.name}}
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+    export default {
+      asyncData({params}){
+        return axios.get(`https://www.easy-mock.com/mock/5c3c576683a6d6013cad25cd/gathering/gathering/${item.id}`).then(res=>{
+          return {item: res.data.data}
+        })
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
